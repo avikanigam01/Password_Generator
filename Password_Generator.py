3a8be81988c14735bd45e77efbd54ddb6e2102f7
Avika Nigam <avikanigam01@gmail.com>
2026-05-30 12:24:40 +0530

    Improve password generator and add length validation
    - Added validation to ensure password length is at least the length of the user's name.
    - Updated password generation to use the user-specified length.
    - Added random digits and special characters to strengthen passwords.

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,11 +1,21 @@
 name= input("Enter name on which you want to generate password: ")
 length= int(input("Enter length of password: "))
-l= list(name)
-password = ""
-import random
-while length>0:
-    random.shuffle(l)
-    a= random.choice(l)
-    password+= a
-    length-=1
-print("your password is: "+ password)
+import string
+slen= len(name)
+if length < len(name):
+    print("Password length should be at least: ", len(name))
+else:
+    l= list(name)
+    password = ""
+    symbols = "!@#$%"
+    import random
+    while slen>0:
+        random.shuffle(l)
+        a= random.choice(l)
+        password+= a
+        slen-=1
+    while len(password)<length:
+        chars = "0123456789@#$%^&*"
+        s= random.choice(chars)
+        password += s
+    print("your password is: "+ password)
