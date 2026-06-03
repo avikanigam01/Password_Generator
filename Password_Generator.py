ffa3702d9d5e9b4f319cfff0c19c80305a34e1aa
Avika Nigam <avikanigam01@gmail.com>
2026-06-04 01:15:19 +0530

    Refactor password generation with exception handling
    Added exception handling and improved input validation to prevent crashes. Enhanced the reliability and usability of the password generator.

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,21 +1,23 @@
 name= input("Enter name on which you want to generate password: ")
-length= int(input("Enter length of password: "))
-import string
-slen= len(name)
-if length < len(name):
-    print("Password length should be at least: ", len(name))
-else:
-    l= list(name)
-    password = ""
-    symbols = "!@#$%"
+try:
+    length= int(input("Enter length of password: "))
     import random
+    slen= len(name)
+    if length < len(name):
+        print("Password length should be at least: ", len(name))
+    else:
+        l= list(name)
+        password = ""
+        symbols = "!@#$%"
     while slen>0:
         random.shuffle(l)
         a= random.choice(l)
         password+= a
         slen-=1
     while len(password)<length:
-        chars = "0123456789@#$%^&*"
-        s= random.choice(chars)
-        password += s
+                chars = "0123456789@#$%^&*"
+                s= random.choice(chars)
+                password += s
     print("your password is: "+ password)
+except ValueError:
+    print("Input should be a number")
